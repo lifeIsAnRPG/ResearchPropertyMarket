9c65ee429833b00a96d9e4f622180a4ae0c83a28
Aleksander <kewgine@gmail.com>
2023-05-06 20:40:45 +0300

    nt-nt

--- dashboard.py
+++ dashboard.py
@@ -2,36 +2,42 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 #Dash Core Components. Этот модуль включает компонент Graph с именем dcc.Graph, который используется для отображения
 # интерактивных графиков.
 import pandas as pd
+import numpy as np
 import plotly.express as px
-
-
-# конструктор dash, отвечает за инициализацию приложения
-app = Dash(__name__)
+import dash_mantine_components as dmc
 link = 'https://docs.google.com/spreadsheets/d/e/' \
-       '2PACX-1vQxxmZm6YG54VucQ9yRgWFQXtOI-RFJ5-sOLT93LpaYGYc-vabL9LOzzkRXX-LmSROTA7hOL1C327nZ/pub?gid=213261502&single=true&output=csv'
+       '2PACX-1vQxxmZm6YG54VucQ9yRgWFQXtOI-RFJ5-sOLT93LpaYGYc-vabL9LOzzkRXX' \
+       '-LmSROTA7hOL1C327nZ/pub?gid=213261502&single=true&output=csv'
 df = pd.read_csv(link)
-developers_count = df.groupby(['city','author']) \
-    .agg({'author_type':'count'}) \
-    .rename({'author_type': 'count'}, axis = 1).sort_values('count', ascending = False)
-# Макет приложения представляет компоненты приложения, которые будут отображаться в веб-браузере, обычно содержащиеся
-# в файле html.Div
-app.layout = html.Div([
-    html.Div(children='Первый трай'),
-    html.Hr(),
-    dcc.RadioItems(options=['Moskva', 'Sankt-Peterburg'], value='Moskva', id='controls-and-radio-item'),
-    dcc.Graph(figure={},id='controls-and-graph')
-])
 
+external_stylesheets = [dmc.theme.DEFAULT_COLORS]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# App layout
+app.layout = dmc.Container([
+    dmc.Title('Dashboard', color="black", size="h3"),
+    dmc.RadioGroup(
+        [dmc.Radio(i, value=i) for i in  ['Moskva', 'Sankt-Peterburg']],
+        id='my-dmc-radio-item',
+        value='Moskva',
+        size="sm"),
+    dmc.Grid([
+        dmc.Col([
+            dcc.Graph(figure={}, id='graph-placeholder')
+            ], span=6),
+        ]),
+], fluid=True)
 @callback(
-    Output(component_id='controls-and-graph', component_property='figure'),
-    Input(component_id='controls-and-radio-item', component_property='value')
+    Output(component_id='graph-placeholder', component_property='figure'),
+    Input(component_id='my-dmc-radio-item', component_property='value')
 )
 def update_graph(col_chosen):
-    fig = px.histogram(developers_count, x=developers_count.index.get_level_values(0)[
-        developers_count.index.get_level_values(1) == col_chosen][:10],
-        y=developers_count[developers_count.index.get_level_values(0) ==
-                                                            col_chosen]['count'][:10])
-    return fig
+        top10_devs = (df.query('city == @col_chosen').author.value_counts(normalize=True) * 100).to_frame().reset_index()[:10]
+        x = top10_devs.author.str.slice(stop=12)
+        x = x.where(x.str.len() != 12,x.str.cat(['..'] * len(top10_devs)))
+        labels = {'x': 'Застройщик','proportion':'% на рынке'}
+        fig = px.histogram(top10_devs, x=x,y='proportion',text_auto ='.2f',labels=labels, title = 'Топ-10 застройщиков')
+        return fig
 
 
 if __name__ == '__main__':
